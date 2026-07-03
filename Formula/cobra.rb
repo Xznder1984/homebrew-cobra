d4608f4ed1d10c36765f1bdf9a9f55c454a8ab77
Razer Albarr <zerobyte@Razers-MacBook-Air.local>
2026-07-03 10:24:46 +0700

    Update SHA256 for v0.1.0 release

--- Formula/cobra.rb
+++ Formula/cobra.rb
@@ -2,7 +2,7 @@ class Cobra < Formula
   desc "Indentation-based programming language compiled to x86-64 native"
   homepage "https://github.com/Xznder1984/Cobra"
   url "https://github.com/Xznder1984/Cobra/releases/download/v0.1.0/cobra-0.1.0-macos-x86_64.tar.gz"
-  sha256 "PLACEHOLDER"
+  sha256 "6d30433f2525b69f74f9dfe5a8614c813eae4fdee1febe1469f822187ae4fc09"
 
   depends_on "make" => :build
 
