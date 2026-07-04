444a945d57fc5a740bf68cd417e04412c1665abd
Razer Albarr <zerobyte@Razers-MacBook-Air.local>
2026-07-04 08:15:10 +0700

    Update macOS Intel SHA after Python-like print/variable changes

--- Formula/cobra.rb
+++ Formula/cobra.rb
@@ -12,7 +12,7 @@ class Cobra < Formula
         sha256 "a571f33bb85d7dba41e031eb219c2b9130e8b78307d110c17af041707de3e80d"
       else
         url "https://github.com/Xznder1984/Cobra/releases/download/v0.2.0/cobra-0.2.0-macos-x86_64.tar.gz"
-        sha256 "686b5a7233cdddc85a9a54d96b1108324f3b3454b111b4cd0842ba5cb20d9f58"
+        sha256 "40f13e563288cedd9f2a5be355d8473181d2455eb8b484bb19f79ea16638aebd"
       end
     end
 
